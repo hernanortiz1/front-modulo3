6145cd4ab75208f0e938275d7177cc18e88c4b4a
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-10-01 18:43:07 -0300

    corregin los classname que tenian class en los iconos

--- src/components/pages/Administrador.jsx
+++ src/components/pages/Administrador.jsx
@@ -1,5 +1,5 @@
 import { Accordion, Table, Button } from "react-bootstrap";
-import React from 'react'
+import React from "react";
 
 const Administrador = () => {
   return (
@@ -8,7 +8,8 @@ const Administrador = () => {
       <article className="my-4">
         <Accordion defaultActiveKey={["0"]} alwaysOpen>
           <Accordion.Item eventKey="0">
-            <Accordion.Header className="Montserrat"><i class="bi bi-database me-2"></i>
+            <Accordion.Header className="Montserrat">
+              <i className="bi bi-database me-2"></i>
               Productos
             </Accordion.Header>
             <Accordion.Body className="row">
@@ -35,16 +36,18 @@ const Administrador = () => {
                 </Button>
               </div>
               <div className="d-flex justify-content-center align-items-center my-3">
-              <Button className="btn-table">Anterior</Button>
-              <span className="mx-3">Página 1 de 3</span>
-              <Button className="btn-table">Siguiente</Button>
-            </div>
+                <Button className="btn-table">Anterior</Button>
+                <span className="mx-3">Página 1 de 3</span>
+                <Button className="btn-table">Siguiente</Button>
+              </div>
             </Accordion.Body>
           </Accordion.Item>
         </Accordion>
         <Accordion defaultActiveKey={["0"]} alwaysOpen className="mt-4">
           <Accordion.Item eventKey="0">
-            <Accordion.Header className="Montserrat"><i class="bi bi-person-fill me-2"></i>Usuarios</Accordion.Header>
+            <Accordion.Header className="Montserrat">
+              <i className="bi bi-person-fill me-2"></i>Usuarios
+            </Accordion.Header>
             <Accordion.Body className="row">
               <div className="col-12 col-md-10">
                 <Table responsive striped bordered hover className="Montserrat">
@@ -62,16 +65,15 @@ const Administrador = () => {
               </div>
               <div className="col-12 col-md-2 text-end text-md-center order-first order-md-0 my-3 my-md-0">
                 <Button className="btn btn-success">
-                  <i class="bi bi-person-plus-fill"></i>
+                  <i className="bi bi-person-plus-fill"></i>
                 </Button>
               </div>
               <div className="d-flex justify-content-center align-items-center my-3">
-              <Button className="btn-table">Anterior</Button>
-              <span className="mx-3">Página 1 de 3</span>
-              <Button className="btn-table">Siguiente</Button>
-            </div>
+                <Button className="btn-table">Anterior</Button>
+                <span className="mx-3">Página 1 de 3</span>
+                <Button className="btn-table">Siguiente</Button>
+              </div>
             </Accordion.Body>
-            
           </Accordion.Item>
         </Accordion>
       </article>

9ea7148cf57dd4ee8bb870c72a0913e8c8de26a9
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-10-01 00:55:09 -0300

    agregue titulo y acordeon de la tabla de productos

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import { useEffect, useState } from "react";
 import Menu from "./components/shared/Menu";
 import Footer from "./components/shared/Footer";
+import Administrador from "./components/pages/Administrador";
 
 function App() {
   const usuarioLogueado = JSON.parse(sessionStorage.getItem("userKey")) || {};
@@ -19,7 +20,7 @@ function App() {
           setUsuarioAdmin={setUsuarioAdmin}
         </Menu>
         <main>
-        
+        <Administrador></Administrador>
         </main>
         <Footer></Footer>
       </BrowserRouter>
--- src/components/pages/Administrador.jsx
+++ src/components/pages/Administrador.jsx
@@ -1,10 +1,48 @@
+import { Accordion, Table, Button } from "react-bootstrap";
 
 const Administrador = () => {
-    return (
-        <>
-            
-        </>
-    );
+  return (
+    <section className="container">
+      <h3 className="mt-5 Montserrat">ADMINISTRADOR</h3>
+      <article className="my-4">
+        <Accordion defaultActiveKey={["0"]} alwaysOpen>
+          <Accordion.Item eventKey="0">
+            <Accordion.Header className="Montserrat">
+              Productos
+            </Accordion.Header>
+            <Accordion.Body className="row">
+              <div className="col-12 col-md-10">
+                <Table
+                responsive
+                  striped
+                  bordered
+                  hover
+                  className="Montserrat"
+                >
+                  <thead>
+                    <tr className="text-center">
+                      <th>Nombre</th>
+                      <th>Categoría</th>
+                      <th>Talle</th>
+                      <th>Color</th>
+                      <th>Precio</th>
+                      <th>Cantidad stock</th>
+                    </tr>
+                  </thead>
+                  <tbody></tbody>
+                </Table>
+              </div>
+              <div className="col-12 col-md-2 text-end order-first order-md-last my-3 my-md-0">
+                <Button className="btn btn-success">
+                  <i className="bi bi-file-earmark-plus"></i>
+                </Button>
+              </div>
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
+      </article>
+    </section>
+  );
 };
 
 export default Administrador;

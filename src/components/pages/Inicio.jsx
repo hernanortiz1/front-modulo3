64164a7d4247d9a937c1c60ba5ebcccd14a88738
Nacho <barrojojoaquin2@gmail.com>
2025-10-01 22:07:42 -0300

    corregi un error de sintaxis con etiquetas links

--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -45,83 +45,92 @@ const Inicio = () => {
           </picture>
         </Carousel.Item>
       </Carousel>
+
       <Container className="my-4">
         <Row data-aos="fade-up" data-aos-delay="0">
           <CardRopa />
         </Row>
+
         {/* TARJETAS */}
         <Row className="m-4 g-3">
-          <Col
-            xs={12}
-            md={6}
-            className="imagen-container"
-            data-aos="fade-right"
-          >
-            <Link>
+          <Col xs={12} md={6} className="imagen-container" data-aos="fade-right">
+            <Link to="/parte-superior">
               <img
-                src="https://images.unsplash.com/photo-1596755094514-f87e34085b2c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2FtaXNhc3xlbnwwfHwwfHx8MA%3D%3D"
+                src="https://images.unsplash.com/photo-1596755094514-f87e34085b2c?w=500&auto=format&fit=crop&q=60"
                 alt=""
                 className="w-100"
               />
+              <span className="texto-sobre-imagen">PARTE SUPERIOR</span>
             </Link>
-            <Link className="texto-sobre-imagen">PARTE SUPERIOR</Link>
           </Col>
 
           <Col xs={12} md={6} className="imagen-container" data-aos="fade-left">
-            <Link>
+            <Link to="/parte-inferior">
               <img
-                src="https://images.unsplash.com/photo-1548883354-7622d03aca27?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHBhbnRhbCVDMyVCM258ZW58MHx8MHx8fDA%3D"
+                src="https://images.unsplash.com/photo-1548883354-7622d03aca27?w=500&auto=format&fit=crop&q=60"
                 alt=""
                 className="w-100"
               />
+              <span className="texto-sobre-imagen">PARTE INFERIOR</span>
             </Link>
-            <Link className="texto-sobre-imagen">PARTE INFERIOR</Link>
           </Col>
         </Row>
+
         <Row className="mt-5">
           <Col
-            className="imagen-container mb-3"
-            data-aos="fade-right"
-            data-aos-duration="1000"
             xs={12}
             sm={12}
             md={4}
+            className="imagen-container mb-3"
+            data-aos="fade-right"
+            data-aos-duration="1000"
           >
-            <Link>
+            <Link to="/remeras-chombas">
               <img
-                src="https://images.unsplash.com/photo-1614495039153-e9cd13240469?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                src="https://images.unsplash.com/photo-1614495039153-e9cd13240469?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.1.0"
                 alt=""
+                className="w-100"
               />
-              <Link className="texto-sobre-imagen">REMERAS Y CHOMBAS</Link>
+              <span className="texto-sobre-imagen">REMERAS Y CHOMBAS</span>
             </Link>
           </Col>
+
           <Col
+            xs={12}
+            sm={12}
+            md={4}
             className="imagen-container mb-3"
             data-aos="zoom-in"
             data-aos-duration="1000"
             data-aos-delay="100"
+          >
+            <Link to="/camisas">
+              <img
+                src="https://images.unsplash.com/photo-1589310243389-96a5483213a8?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.1.0"
+                alt=""
+                className="w-100"
+              />
+              <span className="texto-sobre-imagen">CAMISAS</span>
+            </Link>
+          </Col>
+
+          <Col
             xs={12}
             sm={12}
             md={4}
-          >
-            <Link>
-              <img src="https://images.unsplash.com/photo-1589310243389-96a5483213a8?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
-              <Link className="texto-sobre-imagen">CAMISAS</Link>
-            </Link>
-          </Col>
-          <Col
             className="imagen-container mb-3"
             data-aos="fade-left"
             data-aos-duration="1000"
             data-aos-delay="200"
-            xs={12}
-            sm={12}
-            md={4}
           >
-            <Link>
-              <img src="https://images.unsplash.com/photo-1473966968600-fa801b869a1a?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+            <Link to="/pantalones">
+              <img
+                src="https://images.unsplash.com/photo-1473966968600-fa801b869a1a?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.1.0"
+                alt=""
+                className="w-100"
+              />
+              <span className="texto-sobre-imagen">PANTALONES</span>
             </Link>
-            <Link className="texto-sobre-imagen">PANTALONES</Link>
           </Col>
         </Row>
       </Container>

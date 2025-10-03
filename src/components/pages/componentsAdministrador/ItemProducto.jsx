b86a58b3cdda13a241e37aa1f0ea6d27ecc17d44
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-10-03 09:31:14 -0300

    agregue modal al item usuario de editar

--- src/components/pages/componentsAdministrador/ItemProducto.jsx
+++ src/components/pages/componentsAdministrador/ItemProducto.jsx
@@ -3,26 +3,27 @@ import { Button } from "react-bootstrap";
 import { Link } from "react-router";
 
 const ItemProducto = () => {
+
   return (
-    <tr className="Montserrat">
-      <td className="text-center">1</td>
-      <td className="text-center">Remera OSAKA</td>
-      <td className="text-center">Remeras y chombas</td>
-      <td className="text-center">M</td>
-      <td className="text-center">Gris</td>
-      <td className="text-center">$25.500</td>
-      <td className="text-center">32</td>
-      <td className="text-center">
-        <div className="d-flex gap-1 justify-content-center">
-          <Link className="btn btn-warning me-lg-2">
-            <i className="bi bi-pencil-square"></i>
-          </Link>
-          <Button variant="danger" className="me-lg-2">
-            <i className="bi bi-trash"></i>
-          </Button>
-        </div>
-      </td>
-    </tr>
+      <tr className="Montserrat">
+        <td className="text-center">1</td>
+        <td className="text-center">Remera OSAKA</td>
+        <td className="text-center">Remeras y chombas</td>
+        <td className="text-center">M</td>
+        <td className="text-center">Gris</td>
+        <td className="text-center">$25.500</td>
+        <td className="text-center">32</td>
+        <td className="text-center">
+          <div className="d-flex gap-1 justify-content-center">
+            <Link className="btn btn-warning me-lg-2">
+              <i className="bi bi-pencil-square"></i>
+            </Link>
+            <Button variant="danger" className="me-lg-2">
+              <i className="bi bi-trash"></i>
+            </Button>
+          </div>
+        </td>
+      </tr>
   );
 };
 
--- src/components/pages/componentsAdministrador/ItemUsuario.jsx
+++ src/components/pages/componentsAdministrador/ItemUsuario.jsx
@@ -1,25 +1,99 @@
-import React from 'react';
-import { Button } from "react-bootstrap";
+import React from "react";
+import { Button, Form, Modal } from "react-bootstrap";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 const ItemUsuario = () => {
-    return (
-        <tr className="Montserrat">
-      <td className="text-center">1</td>
-      <td className="text-center">juanperez1</td>
-      <td className="text-center">juanperez@mail.com</td>
-      <td className="text-center">Usuario</td>
-      <td className="text-center">
-        <div className="d-flex gap-1 justify-content-center">
-          <Button className="btn btn-warning me-lg-2">
-            <i className="bi bi-person-fill-gear"></i>
+  const [show, setShow] = useState(false);
+  const handleClose = () => {
+    setShow(false);
+  };
+  const handleShow = () => {
+    setShow(true);
+  };
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  return (
+    <>
+      <tr className="Montserrat">
+        <td className="text-center">1</td>
+        <td className="text-center">juanperez1</td>
+        <td className="text-center">juanperez@mail.com</td>
+        <td className="text-center">Usuario</td>
+        <td className="text-center">
+          <div className="d-flex gap-1 justify-content-center">
+            <Button className="btn btn-warning me-lg-2">
+              <i className="bi bi-person-fill-gear" onClick={handleShow}></i>
+            </Button>
+            <Button variant="danger" className="me-lg-2">
+              <i className="bi bi-person-fill-x"></i>
+            </Button>
+          </div>
+        </td>
+      </tr>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Crea tu cuenta</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form className="raleway" onSubmit={handleSubmit()}>
+            <Form.Group className="mb-3" controlId="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Ej: juanperez@mail.com"
+                {...register("email", {
+                  required: "El email es un dato obligatorio",
+                  pattern: {
+                    value:
+                      /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/,
+                    message:
+                      "El email debe tener un formato valido, por ejemplo juanperez@mail.com",
+                  },
+                })}
+              />
+              <Form.Text className="text-danger">
+                {errors.email?.message}
+              </Form.Text>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="password">
+              <Form.Label>Contraseña</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Ingresa una contraseña"
+                {...register("password", {
+                  required: "La contraseña es un dato obligatorio",
+                  pattern: {
+                    value:
+                      /^(?=.*\d)(?=.*[\u0021-\u002b\u003c-\u0040])(?=.*[A-Z])(?=.*[a-z])\S{8,16}$/,
+                    message:
+                      "La contraseña debe tener entre 8 y 16 caracteres, al menos un dígito, al menos una minúscula, al menos una mayúscula y al menos un caracter no alfanumérico.",
+                  },
+                })}
+              />
+              <Form.Text className="text-danger">
+                {errors.password?.message}
+              </Form.Text>
+            </Form.Group>
+            <Button variant="success" type="submit">
+              Crear cuenta
+            </Button>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="danger" onClick={handleClose}>
+            Cancelar
           </Button>
-          <Button variant="danger" className="me-lg-2">
-            <i className="bi bi-person-fill-x"></i>
-          </Button>
-        </div>
-      </td>
-    </tr>
-    );
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
 };
 
 export default ItemUsuario;

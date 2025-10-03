7eba3856742c90856c7575e6953464b4f32a5ee9
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-10-03 08:44:54 -0300

    importe componentes y agregue rutas

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import { useEffect, useState } from "react";
 import Menu from "./components/shared/Menu";
 import Footer from "./components/shared/Footer";
+import Inicio from "./components/pages/Inicio";
+import Administrador from "./components/pages/Administrador";
+import Error404 from "./components/pages/Error404";
+import Login from "./components/pages/Login";
+import FormularioRopa from "./components/pages/ropa/FormularioRopa";
 
 function App() {
   const usuarioLogueado = JSON.parse(sessionStorage.getItem("userKey")) || {};
@@ -14,12 +19,44 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <Menu>
+        <Menu
           usuarioAdmin={usuarioAdmin}
           setUsuarioAdmin={setUsuarioAdmin}
-        </Menu>
+        ></Menu>
         <main>
-        
+          <Routes>
+            <Route path="/" element={<Inicio />}></Route>
+            {/*  <Route
+              path="/detalle/:id"
+              element={<DetalleProducto></DetalleProducto>}
+            ></Route>*/}
+            <Route path="/login" element={<Login></Login>}></Route>
+
+            {/* <Route
+              path="/administrador"
+              element={<ProtectorAdmin isAdmin={usuarioAdmin}></ProtectorAdmin>}
+            ></Route> */}
+
+            <Route index element={<Administrador></Administrador>}></Route>
+
+            {/* <Route
+                path="crear"
+                element={
+                  <FormularioRopa
+                  titulo={"Crear producto"}  
+                 
+                  ></FormularioRopa>
+                }
+              ></Route> */}
+            <Route
+              path="editar/:id"
+              element={
+                <FormularioRopa titulo={"Editar producto"}></FormularioRopa>
+              }
+            ></Route>
+
+            <Route path="*" element={<Error404></Error404>}></Route>
+          </Routes>
         </main>
         <Footer></Footer>
       </BrowserRouter>

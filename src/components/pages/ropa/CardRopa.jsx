904b2b6214c374a3ca768961dbd3cce249357b95
Nacho <barrojojoaquin2@gmail.com>
2025-10-03 00:32:56 -0300

    Funcion de leer producto y logica de iniciar sesion

--- src/components/pages/ropa/CardRopa.jsx
+++ src/components/pages/ropa/CardRopa.jsx
@@ -1,71 +1,21 @@
 import Card from "react-bootstrap/Card";
-import { Swiper, SwiperSlide } from "swiper/react";
 import { Link } from "react-router";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import { Navigation, Pagination } from "swiper/modules";
 
-const CardRopa = () => {
-  const productos = [
-    {
-      img: "https://equusio.vtexassets.com/arquivos/ids/290550-500-auto?v=638912968771200000&width=500&height=auto&aspect=true",
-      nombre: "Camisa regular 100% algodón OSAKA",
-      precio: "$74.000",
-    },
-    {
-      img: "https://equusio.vtexassets.com/arquivos/ids/290550-500-auto?v=638912968771200000&width=500&height=auto&aspect=true",
-      nombre: "Camisa regular 100% algodón OSAKA",
-      precio: "$74.000",
-    },
-    {
-      img: "https://equusio.vtexassets.com/arquivos/ids/290550-500-auto?v=638912968771200000&width=500&height=auto&aspect=true",
-      nombre: "Camisa regular 100% algodón OSAKA",
-      precio: "$74.000",
-    },
-    {
-      img: "https://equusio.vtexassets.com/arquivos/ids/290550-500-auto?v=638912968771200000&width=500&height=auto&aspect=true",
-      nombre: "Camisa regular 100% algodón OSAKA",
-      precio: "$74.000",
-    },
-    {
-      img: "https://equusio.vtexassets.com/arquivos/ids/290550-500-auto?v=638912968771200000&width=500&height=auto&aspect=true",
-      nombre: "Camisa regular 100% algodón OSAKA",
-      precio: "$74.000",
-    },
-  ];
-
+const CardRopa = ({ ropa }) => {
   return (
     <>
-      <Swiper
-        modules={[Navigation, Pagination]}
-        spaceBetween={20}
-        slidesPerView={4}
-        navigation
-        pagination={{ clickable: true }}
-        style={{ paddingBottom: "40px" }}
-        breakpoints={{
-          0: { slidesPerView: 1 },
-          576: { slidesPerView: 2 },
-          992: { slidesPerView: 4 },
-        }}
-      >
-        {productos.map((ropa, index) => (
-          <SwiperSlide key={index}>
-              <Card className="card-ropa">
-                <Link>
-                <Card.Img variant="top" src={ropa.img} />
-                </Link>
-                <Card.Body>
-                  <Link className="text-decoration-none text-dark Montserrat">
-                    <Card.Title>{ropa.nombre}</Card.Title>
-                    <Card.Text>{ropa.precio}</Card.Text>
-                  </Link>
-                </Card.Body>
-              </Card>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+      <Card className="card-ropa">
+        <Link>
+          <Card.Img variant="top" src={ropa.imagen} />
+        </Link>
+        <Card.Body>
+          <Link className="text-decoration-none text-dark Montserrat">
+            <Card.Title className="Montserrat">{ropa.nombreProducto}</Card.Title>
+            <Card.Text className="Montserrat">{ropa.descripcion}</Card.Text>
+            <Card.Text>${ropa.precio}</Card.Text>
+          </Link>
+        </Card.Body>
+      </Card>
     </>
   );
 };
--- src/helpers/queries.js
+++ src/helpers/queries.js
@@ -1,1 +1,48 @@
 //queries
+const API_URL = import.meta.env.VITE_API_PRODUCTOS;
+const API_USUARIOS = import.meta.env.VITE_API_USUARIOS;
+
+export const crearProducto = async (productoNuevo) => {
+  try {
+    const formData = new FormData();
+    formData.append("nombreProducto", productoNuevo.nombreProducto);
+    formData.append("descripcion", productoNuevo.descripcion);
+    formData.append("precio", productoNuevo.precio);
+    formData.append("imagen", productoNuevo.imagen);
+    formData.append("categoria", productoNuevo.categoria);
+    formData.append("stock", productoNuevo.stock);
+    formData.append("talle", productoNuevo.talle);
+    formData.append("color", productoNuevo.color);
+    formData.append(
+      "fechaUltimoControlStock",
+      productoNuevo.fechaUltimoControlStock
+    );
+    const respuesta = await fetch(API_URL, {});
+  } catch (error) {}
+};
+
+export const obtenerProductos = async () => {
+  try {
+    const respuesta = await fetch(API_URL);
+    return respuesta;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
+
+export const login = async (datosUsuario) => {
+  try {
+    const respuesta = await fetch(API_USUARIOS, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(datosUsuario),
+    });
+    return respuesta;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};

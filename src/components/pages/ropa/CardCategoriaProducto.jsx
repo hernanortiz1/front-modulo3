33ed47dd5b30c25a01bf77127ffe2f38a41f5d57
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-10-04 16:38:55 -0300

    agregue media para dispositivos md y sm

--- src/components/pages/ropa/CardCategoriaProducto.jsx
+++ src/components/pages/ropa/CardCategoriaProducto.jsx
@@ -11,50 +11,42 @@ const CardCategoriaProducto = ({ productos }) => {
     <>
       <Col xs={12} md={6} lg={3} className="mb-3 Montserrat">
         <Link to={`/detalle/${productos._id}`} className="text-decoration-none">
-          <Card className="h-100 d-flex flex-column p-0 cardCategorias shadow">
+          <Card className="d-flex flex-column p-0 cardCategorias shadow">
             <div className="cardCategoriasImgWrapper">
-              <Card.Img
-                variant="top"
+              <img
                 src={productos.imagen}
                 alt={productos.nombreProducto}
                 className="cardCategoriasImg"
               />
-              <div>
-                
+              <div className="cardOverlay">
+                <strong>Talle:</strong> {productos.talle} |{" "}
+                <strong>Color:</strong> {productos.color}
               </div>
             </div>
 
-            <Card.Body className="flex-grow-1 d-flex flex-column justify-content-between h-100">
-              <div className="flex-grow-1 p-0">
-                <ul className="list-unstyled">
-                  <li className="mb-1">
-                    <strong className="fs-4">{productos.nombreProducto}</strong>
-                  </li>
-                  <li className="mb-1 fs-5">
-                    <strong>$ {productos.precio}</strong>
-                  </li>
-                  <li className="mb-1">
-                    <small className="text-primary">
-                      ${precioDescuento(productos.precio, 10)} con Transferencia
-                    </small>
-                  </li>
-                  <li className="mb-1">
-                     3 cuotas sin interés de{" "}
-                    <strong>${calcularCuotas(productos.precio, 3)}</strong>
-                  </li>
-
-                  <li className="mb-1">
-                    <strong>Talle:</strong> {productos.talle}
-                    {" | "}
-                    <strong>Color:</strong> {productos.color}
-                  </li>
-                  <li className="mb-1">
-                    <small>
-                      {productos.descripcion.substring(0, 50) + "..."}
-                    </small>
-                  </li>
-                </ul>
-              </div>
+            <Card.Body className="cardCategoriasBody">
+              <ul className="list-unstyled mb-0">
+                <li className="mb-1">
+                  <strong className="fs-4">{productos.nombreProducto}</strong>
+                </li>
+                <li className="mb-1 fs-5">
+                  <strong>$ {productos.precio}</strong>
+                </li>
+                <li className="mb-1">
+                  <small className="text-primary">
+                    ${precioDescuento(productos.precio, 10)} con Transferencia
+                  </small>
+                </li>
+                <li className="mb-1">
+                  3 cuotas sin interés de{" "}
+                  <strong>${calcularCuotas(productos.precio, 3)}</strong>
+                </li>
+                <li className="mb-1">
+                  <small>
+                    {productos.descripcion.substring(0, 50) + "..."}
+                  </small>
+                </li>
+              </ul>
             </Card.Body>
           </Card>
         </Link>

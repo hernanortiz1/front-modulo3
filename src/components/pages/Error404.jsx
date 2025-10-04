082bb56189c30b3fd990c87c2b09abd50701133c
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-10-04 10:55:08 -0300

    maquetado de la pagina error404

--- src/components/pages/Error404.jsx
+++ src/components/pages/Error404.jsx
@@ -1,11 +1,23 @@
-
+import errorImagen from "../../assets/sobreNosotros/error404.png";
+import "../../index.css";
+import { Link } from "react-router";
+import React from "react";
 
 const Error404 = () => {
-    return (
-        <>
-            
-        </>
-    );
+  return (
+    <section className="container-fluid mb-5 d-block align-content-center">
+      <div className="d-flex justify-content-center mt-5 col-12">
+        <img
+          src={errorImagen}
+          alt="Pagina no encontrada"
+          className="img-fluid"
+        />
+      </div>
+      <div className="d-flex justify-content-center col-12">
+        <Link className="btn-error Montserrat">Volver al Inicio</Link>
+      </div>
+    </section>
+  );
 };
 
 export default Error404;

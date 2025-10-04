88b021651f0e044c985c3cce1d1c3b3ebd83fd5f
Lucasfi <lukafigueroa64@gmail.com>
2025-10-03 22:04:04 -0300

    Agregue maquetado de seccion con formulario en formularioRopa

--- src/components/pages/ropa/FormularioRopa.jsx
+++ src/components/pages/ropa/FormularioRopa.jsx
@@ -1,11 +1,55 @@
-
+import { Form, Button } from "react-bootstrap";
+import React from "react";
 
 const FormularioRopa = () => {
-    return (
-        <>
-            
-        </>
-    );
+  return (
+    <section className="container">
+      <h1 className="display-4 mt-5">Crear Producto</h1>
+      <hr />
+      <Form className="my-4">
+        <Form.Group className="mb-3" controlId="formNombreProdcuto">
+          <Form.Label>Nombre producto*</Form.Label>
+          <Form.Control type="text" placeholder="Ej: Remera Levis" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formPrecio">
+          <Form.Label>Precio*</Form.Label>
+          <Form.Control type="number" placeholder="Ej: 50" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formImagen">
+          <Form.Label>Imagen URL*</Form.Label>
+          <Form.Control type="file" accept="image/*" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formPrecio">
+          <Form.Label>Categoría*</Form.Label>
+          <Form.Select placeholder="Debe seleccionar una categora">
+            <option value="">Seleccione una opcion</option>
+            <option value="Infusiones">Infusiones</option>
+            <option value="Batidos">Batidos</option>
+            <option value="dulce">Dulce</option>
+            <option value="salado">Salado</option>
+          </Form.Select>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formImagen">
+          <Form.Label>Descripción breve*</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Ej: Una taza de café suave y aromático."
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formImagen">
+          <Form.Label>Descripción Amplia*</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Ej: El café americano es una bebida caliente que consiste en un espresso diluido con agua caliente, lo que resulta en una taza de café suave y aromático. Es una opción popular para aquellos que prefieren un café menos intenso que el espresso tradicional. Perfecto para disfrutar en cualquier momento del día."
+            as="textarea"
+          />
+        </Form.Group>
+        <Button type="submit" variant="success">
+          Crear producto
+        </Button>
+      </Form>
+    </section>
+  );
 };
 
 export default FormularioRopa;

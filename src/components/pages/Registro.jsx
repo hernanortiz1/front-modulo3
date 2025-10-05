60200f99e05a67a566a3caa918ab6181d51c5862
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-10-05 11:17:53 -0300

    maquetado del username

--- src/components/pages/Registro.jsx
+++ src/components/pages/Registro.jsx
@@ -1,18 +1,54 @@
 import React from "react";
+import { Form, Button } from "react-bootstrap";
+import { useForm } from "react-hook-form";
 
 const Registro = () => {
+
+    const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
     return (
         <>
         <section className="py-3 colorNavbarFooter text-light">
         <h1 className=" Montserrat text-center">CREA TU CUENTA</h1>
       </section>
-      <section className="container-fluid">
-        <Form className="raleway" onSubmit={handleSubmit(crearCuenta)}>
+      <section className="container-fluid my-5">
+        <Form className="Montserrat" onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="nombreUsuario">
+              <Form.Label>Nombre del usuario</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Ej: juanperez01"
+                required
+                maxLength={100}
+                {...register("nombreUsuario", {
+                  required: "El nombre del usuario es un dato obligatorio",
+              minLength: {
+                value: 3,
+                message:
+                  "El nombre del usuario debe tener al menos 3 caracteres",
+              },
+              maxLength: {
+                value: 100,
+                message:
+                  "El nombre del usuario debe tener como maximo 100 caracteres",
+              },
+                })}
+              />
+              <Form.Text className="text-danger">
+                {errors.nombreUsuario?.message}
+              </Form.Text>
+            </Form.Group>
             <Form.Group className="mb-3" controlId="email">
               <Form.Label>Email</Form.Label>
               <Form.Control
                 type="email"
                 placeholder="Ej: juanperez@mail.com"
+                required
+                maxLength={100}
                 {...register("email", {
                   required: "El email es un dato obligatorio",
                   pattern: {
@@ -32,6 +68,8 @@ const Registro = () => {
               <Form.Control
                 type="password"
                 placeholder="Ingresa una contraseña"
+                required
+                maxLength={40}
                 {...register("password", {
                   required: "La contraseña es un dato obligatorio",
                   pattern: {

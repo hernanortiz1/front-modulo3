8f81aece26558c4208b3a717fb752d7faeb7153e
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-10-05 11:02:19 -0300

    maquetado del form de los campos email y password

--- src/components/pages/Registro.jsx
+++ src/components/pages/Registro.jsx
@@ -3,7 +3,54 @@ import React from "react";
 const Registro = () => {
     return (
         <>
-
+        <section className="py-3 colorNavbarFooter text-light">
+        <h1 className=" Montserrat text-center">CREA TU CUENTA</h1>
+      </section>
+      <section className="container-fluid">
+        <Form className="raleway" onSubmit={handleSubmit(crearCuenta)}>
+            <Form.Group className="mb-3" controlId="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Ej: juanperez@mail.com"
+                {...register("email", {
+                  required: "El email es un dato obligatorio",
+                  pattern: {
+                    value:
+                      /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/,
+                    message:
+                      "El email debe tener un formato valido, por ejemplo juanperez@mail.com",
+                  },
+                })}
+              />
+              <Form.Text className="text-danger">
+                {errors.email?.message}
+              </Form.Text>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="password">
+              <Form.Label>Contraseña</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Ingresa una contraseña"
+                {...register("password", {
+                  required: "La contraseña es un dato obligatorio",
+                  pattern: {
+                    value:
+                      /^(?=.*\d)(?=.*[\u0021-\u002b\u003c-\u0040])(?=.*[A-Z])(?=.*[a-z])\S{8,16}$/,
+                    message:
+                      "La contraseña debe tener entre 8 y 16 caracteres, al menos un dígito, al menos una minúscula, al menos una mayúscula y al menos un caracter no alfanumérico.",
+                  },
+                })}
+              />
+              <Form.Text className="text-danger">
+                {errors.password?.message}
+              </Form.Text>
+            </Form.Group>
+            <Button variant="success" type="submit">
+              Registrarse
+            </Button>
+          </Form>
+      </section>
         </>
     );
 };

691306dc9fc262e189fa62ac11d43da3a23a228d
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-10-06 08:44:34 -0300

    saque el grid de categorias y agregue los filtors en el map de cada una

--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -122,6 +122,69 @@ const Inicio = () => {
               992: { slidesPerView: 4 },
             }}
           >
+            {remeras.map((ropa) => (
+              <SwiperSlide key={ropa.id}>
+                <CardRopa ropa={ropa} />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+        <div data-aos="fade-down" data-aos-delay="0">
+          <Swiper
+            modules={[Navigation, Pagination]}
+            spaceBetween={20}
+            slidesPerView={4}
+            navigation
+            pagination={{ clickable: true }}
+            style={{ paddingBottom: "40px" }}
+            breakpoints={{
+              0: { slidesPerView: 1 },
+              576: { slidesPerView: 2 },
+              992: { slidesPerView: 4 },
+            }}
+          >
+            {camisas.map((ropa) => (
+              <SwiperSlide key={ropa.id}>
+                <CardRopa ropa={ropa} />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+        <div data-aos="fade-down" data-aos-delay="0">
+          <Swiper
+            modules={[Navigation, Pagination]}
+            spaceBetween={20}
+            slidesPerView={4}
+            navigation
+            pagination={{ clickable: true }}
+            style={{ paddingBottom: "40px" }}
+            breakpoints={{
+              0: { slidesPerView: 1 },
+              576: { slidesPerView: 2 },
+              992: { slidesPerView: 4 },
+            }}
+          >
+            {buzos.map((ropa) => (
+              <SwiperSlide key={ropa.id}>
+                <CardRopa ropa={ropa} />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+        <div data-aos="fade-down" data-aos-delay="0">
+          <Swiper
+            modules={[Navigation, Pagination]}
+            spaceBetween={20}
+            slidesPerView={4}
+            navigation
+            pagination={{ clickable: true }}
+            style={{ paddingBottom: "40px" }}
+            breakpoints={{
+              0: { slidesPerView: 1 },
+              576: { slidesPerView: 2 },
+              992: { slidesPerView: 4 },
+            }}
+          >
             {abrigosCamperas.map((ropa) => (
               <SwiperSlide key={ropa.id}>
                 <CardRopa ropa={ropa} />
@@ -143,106 +206,97 @@ const Inicio = () => {
               992: { slidesPerView: 4 },
             }}
           >
-            {productos.map((ropa) => (
+            {pantalones.map((ropa) => (
               <SwiperSlide key={ropa.id}>
                 <CardRopa ropa={ropa} />
               </SwiperSlide>
             ))}
           </Swiper>
         </div>
-
-        {/* TARJETAS */}
-        <Row className="m-4 g-3">
-          <Col
-            xs={12}
-            md={6}
-            className="imagen-container box-shadow"
-            data-aos="fade-right"
+        <div data-aos="fade-down" data-aos-delay="0">
+          <Swiper
+            modules={[Navigation, Pagination]}
+            spaceBetween={20}
+            slidesPerView={4}
+            navigation
+            pagination={{ clickable: true }}
+            style={{ paddingBottom: "40px" }}
+            breakpoints={{
+              0: { slidesPerView: 1 },
+              576: { slidesPerView: 2 },
+              992: { slidesPerView: 4 },
+            }}
           >
-            <Link to="/parte-superior">
-              <img
-                src="https://images.unsplash.com/photo-1596755094514-f87e34085b2c?w=500&auto=format&fit=crop&q=60"
-                alt="Parte Superior Ropa"
-                className="w-100"
-              />
-              <span className="texto-sobre-imagen">PARTE SUPERIOR</span>
-            </Link>
-          </Col>
-
-          <Col
-            xs={12}
-            md={6}
-            className="imagen-container box-shadow"
-            data-aos="fade-left"
+            {bermudas.map((ropa) => (
+              <SwiperSlide key={ropa.id}>
+                <CardRopa ropa={ropa} />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+        <div data-aos="fade-down" data-aos-delay="0">
+          <Swiper
+            modules={[Navigation, Pagination]}
+            spaceBetween={20}
+            slidesPerView={4}
+            navigation
+            pagination={{ clickable: true }}
+            style={{ paddingBottom: "40px" }}
+            breakpoints={{
+              0: { slidesPerView: 1 },
+              576: { slidesPerView: 2 },
+              992: { slidesPerView: 4 },
+            }}
           >
-            <Link to="/parte-inferior">
-              <img
-                src="https://images.unsplash.com/photo-1548883354-7622d03aca27?w=500&auto=format&fit=crop&q=60"
-                alt="Parte Inferior Ropa"
-                className="w-100"
-              />
-              <span className="texto-sobre-imagen">PARTE INFERIOR</span>
-            </Link>
-          </Col>
-        </Row>
-
-        <Row className="mt-5">
-          <Col
-            xs={12}
-            sm={12}
-            md={4}
-            className="imagen-container mb-3 box-shadow"
-            data-aos="fade-right"
-            data-aos-duration="1000"
+            {shortsBanio.map((ropa) => (
+              <SwiperSlide key={ropa.id}>
+                <CardRopa ropa={ropa} />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+        <div data-aos="fade-down" data-aos-delay="0">
+          <Swiper
+            modules={[Navigation, Pagination]}
+            spaceBetween={20}
+            slidesPerView={4}
+            navigation
+            pagination={{ clickable: true }}
+            style={{ paddingBottom: "40px" }}
+            breakpoints={{
+              0: { slidesPerView: 1 },
+              576: { slidesPerView: 2 },
+              992: { slidesPerView: 4 },
+            }}
           >
-            <Link to="/remeras-chombas box-shadow">
-              <img
-                src="https://images.unsplash.com/photo-1614495039153-e9cd13240469?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.1.0"
-                alt="Remeras y Chombas"
-                className="w-100"
-              />
-              <span className="texto-sobre-imagen">REMERAS Y CHOMBAS</span>
-            </Link>
-          </Col>
-
-          <Col
-            xs={12}
-            sm={12}
-            md={4}
-            className="imagen-container mb-3 box-shadow"
-            data-aos="zoom-in"
-            data-aos-duration="1000"
-            data-aos-delay="100"
+            {gorras.map((ropa) => (
+              <SwiperSlide key={ropa.id}>
+                <CardRopa ropa={ropa} />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+        <div data-aos="fade-down" data-aos-delay="0">
+          <Swiper
+            modules={[Navigation, Pagination]}
+            spaceBetween={20}
+            slidesPerView={4}
+            navigation
+            pagination={{ clickable: true }}
+            style={{ paddingBottom: "40px" }}
+            breakpoints={{
+              0: { slidesPerView: 1 },
+              576: { slidesPerView: 2 },
+              992: { slidesPerView: 4 },
+            }}
           >
-            <Link to="/camisas">
-              <img
-                src="https://images.unsplash.com/photo-1589310243389-96a5483213a8?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.1.0"
-                alt="Camisas"
-                className="w-100"
-              />
-              <span className="texto-sobre-imagen">CAMISAS</span>
-            </Link>
-          </Col>
-
-          <Col
-            xs={12}
-            sm={12}
-            md={4}
-            className="imagen-container mb-3 box-shadow"
-            data-aos="fade-left"
-            data-aos-duration="1000"
-            data-aos-delay="200"
-          >
-            <Link to="/pantalones">
-              <img
-                src="https://images.unsplash.com/photo-1473966968600-fa801b869a1a?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.1.0"
-                alt="Pantalones"
-                className="w-100"
-              />
-              <span className="texto-sobre-imagen">PANTALONES</span>
-            </Link>
-          </Col>
-        </Row>
+            {anteojos.map((ropa) => (
+              <SwiperSlide key={ropa.id}>
+                <CardRopa ropa={ropa} />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
       </Container>
       <section className="mt-3">
         <BannerPublicidad />

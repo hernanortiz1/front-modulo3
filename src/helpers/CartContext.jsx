27ca150d388e2bd74a5b36bb359a606967356989
Lucasfi <lukafigueroa64@gmail.com>
2025-10-08 15:58:06 -0300

    Agregue funcion para agregar productos al carrito

--- src/helpers/CartContext.jsx
+++ src/helpers/CartContext.jsx
@@ -27,5 +27,29 @@ export const CartProvider = ({ children }) => {
     localStorage.setItem("lannister-cart", JSON.stringify(cartItems));
   }, [cartItems]);
 
+  const addToCart = (product) => {
+    setCartItems((prevItems) => {
+      const existingItem = prevItems.find(
+        (item) => item.id === product.id && item.size === product.size
+      );
+
+      if (existingItem) {
+        return prevItems.map((item) =>
+          item.id === product.id && item.size === product.size
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        );
+      } else {
+        return [...prevItems, { ...product, quantity: 1 }];
+      }
+    });
+  };
+
+  const value = {
+    cartItems,
+    addToCart,
+  };
+
+
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };

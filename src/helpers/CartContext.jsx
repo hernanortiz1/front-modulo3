53dab276f44a8f791c6317b5fe883435c2612c18
Lucasfi <lukafigueroa64@gmail.com>
2025-10-08 15:53:24 -0300

    Agregue archivo querie para logica de carrito con ContextApi

---
+++ src/helpers/CartContext.jsx
@@ -0,0 +1,12 @@
+// helpers/CartContext.jsx
+import React, { createContext, useContext, useState, useEffect } from "react";
+
+const CartContext = createContext();
+
+export const useCart = () => {
+  const context = useContext(CartContext);
+  if (!context) {
+    throw new Error("useCart debe usarse dentro de un CartProvider");
+  }
+  return context;
+};

4cbb79ad10baf9edb259254bdb5b8cd7dca51e20
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-10-12 19:42:47 -0300

    organice un poco mejor y mas distribuido la informacion de talle, categorias, color y descripcion

--- src/components/pages/DetalleProducto.jsx
+++ src/components/pages/DetalleProducto.jsx
@@ -4,8 +4,15 @@ import { ShoppingCart, Heart, ShoppingBag } from "lucide-react";
 import { useParams } from "react-router";
 import { obtenerProductosPorId } from "../../helpers/queries";
 import { useEffect, useState } from "react";
+import {
+  precioDescuento,
+  calcularCuotas,
+  formatearPrecio,
+  precioSinImpuestos,
+} from "../pages/categorias/funcion/operaciones.js";
+
 const DetalleProducto = () => {
-  const [producto, setProducto] = useState([]);
+  const [producto, setProducto] = useState({});
   const [favorito, setFavorito] = useState(false);
   const { id } = useParams();
 
@@ -42,16 +49,7 @@ const DetalleProducto = () => {
                 {/* Categoría y Favorito */}
                 <div className="d-flex justify-content-between align-items-center mb-3">
                   <span className="badge colorNavbarFooter">
-                    Categoria: {producto.categoria}
-                  </span>
-                  <span className="badge colorNavbarFooter">
-                    Stock: {producto.stock}
-                  </span>
-                  <span className="badge colorNavbarFooter">
-                    Color: {producto.color}
-                  </span>
-                  <span className="badge colorNavbarFooter">
-                    Talle: {producto.talle}
+                    {producto.categoria}
                   </span>
                   <Button
                     variant="link"
@@ -69,15 +67,71 @@ const DetalleProducto = () => {
                 {/* Título */}
                 <h2 className="Montserrat mb-3">{producto.nombreProducto}</h2>
 
-                {/* Precio */}
-                <h3 className="Montserrat mb-3">${producto.precio}</h3>
-
                 {/* Descripción */}
-                <div className="mb-4">
-                  <span className="badge colorNavbarFooter mb-2">
-                    Descripción:
-                  </span>
+                <div className="mb-4 d-flex flex-column">
                   <p className="text-muted mb-0">{producto.descripcion}</p>
+                </div>
+                <div className="d-flex justify-content-between my-4">
+                  <p className="badge colorNavbarFooter w-25">
+                    Stock: {producto.stock}
+                  </p>
+                  <p className="badge colorNavbarFooter w-25">
+                    Color: {producto.color}
+                  </p>
+                  <p className="badge colorNavbarFooter w-25">
+                    Talle: {producto.talle}
+                  </p>
+                </div>
+                {/* Precio */}
+                <h3 className="Montserrat mb-3">
+                  ${formatearPrecio(producto.precio)}
+                </h3>
+
+                <div>
+                  <ul className="list-unstyled mb-0">
+                    <li className="mb-1">
+                      <small className="text-primary">
+                        ${" "}
+                        {formatearPrecio(precioDescuento(producto.precio, 10))}{" "}
+                        con Transferencia
+                      </small>
+                    </li>
+                    <li className="mb-1">
+                      3 cuotas sin interés de{" "}
+                      <strong>
+                        $ {formatearPrecio(calcularCuotas(producto.precio, 3))}
+                      </strong>
+                    </li>
+                    <li className="mb-1">
+                      <small className="textoPequenio d-block">
+                        Precio sin impuestos nacionales ${" "}
+                        {formatearPrecio(precioSinImpuestos(producto.precio))}
+                      </small>
+                    </li>
+                  </ul>
+                </div>
+
+                <div class="d-flex justify-content-center pe-3 ps-5">
+                  <img
+                    src="../"
+                    alt=""
+                    class="border border-3 border-dark-subtle w-25 img-fluid me-1 rounded-3 d-block"
+                  />
+                  <img
+                    src="../img/productonotebook/2.png"
+                    alt=""
+                    class="border border-3 border-dark-subtle w-25 img-fluid me-1 rounded-3 d-block"
+                  />
+                  <img
+                    src="../img/productonotebook/3.png"
+                    alt=""
+                    class="border border-3 border-dark-subtle w-25 img-fluid me-1 rounded-3 d-block"
+                  />
+                  <img
+                    src="../img/productonotebook/4.png"
+                    alt=""
+                    class="border border-3 border-dark-subtle w-25 img-fluid me-1 rounded-3 d-block"
+                  />
                 </div>
 
                 <hr />
